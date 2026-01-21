883c1b3b5c8ffd99b2a83b9cfb921260892fc29c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2026-01-21 14:26:27 +0100

    [IMP] account_invoice_transmit_peppol: Speedup job creation
    Makes job submition indepotent and split into smaller transaction to avoid to lock too much account.move

--- account_invoice_transmit_peppol/models/account_move.py
+++ account_invoice_transmit_peppol/models/account_move.py
@@ -8,7 +8,16 @@ from odoo.addons.queue_job.job import identity_exact
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def _batch_transmit_invoice_by_peppol(self):
+    def _is_transmissible_by_peppol(self):
+        """Check if the invoice can be sent by peppol"""
+        self.ensure_one()
+        return (
+            not self.is_move_sent
+            and not self.invoice_exported
+            and self.transmit_method_code == "peppol"
+        )
+
+    def _batch_transmit_invoice_by_peppol(self, commit_interval=10):
         """Mass sending by peppol
 
         Since sending by Peppol can be time consuming due to the
@@ -17,21 +26,33 @@ class AccountMove(models.Model):
 
         """
         result = []
+        cpt = 0
         for invoice in self:
-            description = _(
-                "Generating invoice for peppol sending: %(name)s",
-                name=invoice.name,
-            )
-            invoice.with_delay(
-                description=description,
-                identity_key=identity_exact,
-                priority=40,
-                channel="root.invoice_transmit.peppol",
-            )._transmit_invoice_by_peppol()
+            cpt += 1
+            if not invoice._is_transmissible_by_peppol():
+                description = _(
+                    "Invoice already sent to peppol: %(name)s",
+                    name=invoice.name,
+                )
+            else:
+                description = _(
+                    "Generating invoice for peppol sending: %(name)s",
+                    name=invoice.name,
+                )
+                invoice.with_delay(
+                    description=description,
+                    identity_key=identity_exact,
+                    priority=40,
+                    channel="root.invoice_transmit.peppol",
+                )._transmit_invoice_by_peppol()
+            if cpt % commit_interval == 0:
+                self.env.cr.commit()
             result.append(description)
+
         return "\n".join(result)
 
     def _transmit_invoice_by_peppol(self):
         """Sending by peppol"""
+        invoices = self.filtered(self._is_transmissible_by_peppol)
         invoices = self._transmit_invoice("peppol")
         return invoices.peppol_export_invoice()
